3bd26bfb42367929e9a7bcfb979eb3eb0ffcfd01
Petherson Erasmo da Silva <petherson_erasmo@hotmail.com>
2022-03-05 20:22:23 -0300

    Implement the third topic

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
 const fs = require('fs')
 
-function handleErro(erro) {
+function extractsLink (text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+  const resultArray = []
+  let temp
+  while ((temp = regex.exec(text)) !== null) {
+    resultArray.push({ [temp[1]]: temp[2] })
+  }
+
+  return resultArray
+}
+
+function handleErro (erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
 
 /* code async */
-async function getFile(filePath) {
+async function getFile (filePath) {
   const encoding = 'utf-8'
   try {
     const text = await fs.promises.readFile(filePath, encoding)
-    console.log(chalk.green(text))
+    console.log(extractsLink(text))
   } catch (erro) {
     handleErro(erro)
   } finally {
-    console.log(chalk.yellow('operação concluída'));
+    console.log(chalk.yellow('operação concluída'))
   }
 }
 
