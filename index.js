cb12b7e3fa5c1f980b653037b83c3a1a1e44f4c9
Petherson Erasmo da Silva <petherson_erasmo@hotmail.com>
2022-03-05 18:50:57 -0300

    Implement the second topic

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const chalk = require('chalk');
+const fs = require('fs')
 
-console.log(chalk.blue('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function handleErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
 
-console.log(texto(paragrafo));
+/* code async */
+async function getFile(filePath) {
+  const encoding = 'utf-8'
+  try {
+    const text = await fs.promises.readFile(filePath, encoding)
+    console.log(chalk.green(text))
+  } catch (erro) {
+    handleErro(erro)
+  } finally {
+    console.log(chalk.yellow('operação concluída'));
+  }
+}
+
+/* linked list */
+// function getFile (filePath) {
+//   const encoding = 'utf-8'
+//   fs.promises
+//     .readFile(filePath, encoding)
+//     .then((data) => chalk.green(console.log(data)))
+//     .catch((erro) => handleErro(erro))
+// }
+
+/* code async */
+// function getFile(filePath) {
+//   const encoding = 'utf-8'
+//   fs.readFile(filePath, encoding, (erro, data) => {
+//     if (erro) {
+//       handleErro(erro)
+//     } else {
+//       console.log(chalk.green(data))
+//     }
+//   })
+// }
+
+getFile('./arquivos/texto1.md')
